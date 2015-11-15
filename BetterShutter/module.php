229751706e7b0168725ee60b1f0c7dfb5d505c95
Thomas Hoechsmann <thoechsmann@iwef.de>
2015-11-15 20:54:08 +0100

    d

--- BetterShutter/module.php
+++ BetterShutter/module.php
@@ -30,6 +30,7 @@ class BetterShutter extends BetterBase {
 
         $limitId = $this->RegisterVariableInteger("positionLimit", "Limit");
         $twighlightCheckId = $this->RegisterVariableBoolean("twighlightCheck", "Dämmerungsautomatik");
+        $this->RegisterVariableBoolean("shouldBeDown", "shouldBeDown");
 
         // Scheduled Event
         $scheduler = $this->RegisterScheduler("Wochenplan");
@@ -43,11 +44,14 @@ class BetterShutter extends BetterBase {
         IPS_SetIcon($scheduler, "Calendar");
         IPS_SetPosition($scheduler, 5);
         IPS_SetEventScheduleGroup($scheduler, 0, 127); // Mo - Fr (1 + 2 + 4 + 8 + 16)
-        IPS_SetEventScheduleAction($scheduler, 0, "Offen", 0x00FF00, "BH_SetMode(\$_IPS['TARGET'], 1);");
-        IPS_SetEventScheduleAction($scheduler, 1, "Geschlossen", 0x0000FF, "BH_SetMode(\$_IPS['TARGET'], 2);");
+        IPS_SetEventScheduleAction($scheduler, 0, "Offen", 0x00FF00, "BH_OpenShutter(\$_IPS['TARGET']);");
+        IPS_SetEventScheduleAction($scheduler, 1, "Geschlossen", 0x0000FF, "BH_CloseShutter(\$_IPS['TARGET']);");
 
         $downTriggerId = $this->RegisterTrigger("upDownTrigger", $this->ReadPropertyInteger("upDownId"), 'BS_DownEvent($_IPS[\'TARGET\']);', 4);
         IPS_SetEventTriggerValue($downTriggerId, true);
+
+        $downTriggerId = $this->RegisterTrigger("openCloseTrigger", $this->ReadPropertyInteger("windowId"), 'BS_WindowEvent($_IPS[\'TARGET\']);', 1);
+
 	}
 
     public function RequestAction($Ident, $Value) 
@@ -61,9 +65,51 @@ class BetterShutter extends BetterBase {
         }
     }
 
+    public function OpenShutter()
+    {
+        $shouldBeDownId = $this->GetIDForIdent("shouldBeDown");
+        $shouldBeDown = IPS_SetValue($shouldBeDownId, false);
+
+        // send close to KNX
+        $upDownId = $this->ReadPropertyInteger("upDownId")
+        EIB_Switch(IPS_GetParent($upDownId), false;
+    }
+
+    public function CloseShutter()
+    {   
+        $shouldBeDownId = $this->GetIDForIdent("shouldBeDown");
+        $shouldBeDown = IPS_SetValue($shouldBeDownId, true);
+
+        // send close to KNX
+        $upDownId = $this->ReadPropertyInteger("upDownId")
+        EIB_Switch(IPS_GetParent($upDownId), true);
+    }
+
     public function DownEvent()
     {
-        IPS_LogMessage("BetterShutter", "DownEvent");
+        $windowId = $this->ReadPropertyInteger("windowId");
+        if(GetValue($id) == false) // window closed
+        {
+            return;
+        }
+
+        IPS_LogMessage("BetterShutter", "Window opened. Limiting shutter movement");
+
+        $positionId = $this->ReadPropertyInteger("positionId"));
+        $positionLimit = $this->ReadPropertyInteger("positionLimit"));
+        EIB_Scale(IPS_GetParent($positionId, $positionLimit);
+    }
+
+    public function WindowEvent()
+    {
+        $id = $this->ReadPropertyInteger("windowId");
+        if(GetValue($id) == false) // window closed
+        {
+            return;
+        }
+
+        IPS_LogMessage("BetterShutter", "Window opened. Limiting shutter movement");
+        EIB_Scale(IPS_GetParent($this->ReadPropertyInteger("positionId")), $this->ReadPropertyInteger("positionLimit")));
     }
 
 }
