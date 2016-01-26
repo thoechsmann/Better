89df8992d6a506a38cb9c4d8facb20263fc4b4ed
Thomas Hoechsmann <thoechsmann@iwef.de>
2016-01-26 23:26:45 +0100

    light

--- Variable.php
+++ Variable.php
@@ -12,12 +12,14 @@ class Variable  {
 
     public function RegisterVariableInteger($name, $profile = "", $position = 0)
     {
-        $this->module->RegisterVariableInteger($this->ident, $name, $profile, $position);
+        $id = $this->module->RegisterVariableInteger($this->ident, $name, $profile, $position);
+        IPS_SetName($id, $name);
     }
 
    public function RegisterVariableBoolean($name, $profile = "", $position = 0)
     {
         $this->module->RegisterVariableBoolean($this->ident, $name, $profile, $position);
+        IPS_SetName($id, $name);
     }
 
     public function Ident()
