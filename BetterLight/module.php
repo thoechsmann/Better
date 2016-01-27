049b1ee10a6e09d3984a2750fefc0fd42006034e
Thomas Hoechsmann <thoechsmann@iwef.de>
2016-01-28 00:29:07 +0100

    light

--- BetterLight/module.php
+++ BetterLight/module.php
@@ -416,7 +416,9 @@ class BetterLight extends BetterBase {
             if($sceneName != "")
             {
                 $sceneColor = intval($this->SceneColorProperties()->ValueAt($sceneNumber), 0);
-                IPS_LogMessage("BetterLight", "CreateSceneProfile sceneNumber = $sceneNumber, sceneColor = $sceneColor");
+                IPS_LogMessage("BetterLight", 
+                    "CreateSceneProfile sceneNumber = $sceneNumber, sceneColor = $sceneColor stringColor = " .
+                    $this->SceneColorProperties()->ValueAt($sceneNumber));
                 IPS_SetVariableProfileAssociation($this->SceneProfileString(), $sceneNumber, $sceneName, "", $sceneColor);
             }
         }
