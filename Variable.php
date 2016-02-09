04f03b3b1fcde60f652f33fe7078e256345b9936
Thomas Hoechsmann <thoechsmann@iwef.de>
2016-02-09 22:45:07 +0100

    IPSObject

--- Variable.php
+++ Variable.php
@@ -294,8 +294,8 @@ class IPSEvent extends IPSObject
             IPS_SetIdent($id, $this->Ident());            
         }
 
-        IPS_SetName($id, $name);
-        IPS_SetPosition($id, $position);
+        // IPS_SetName($id, $name);
+        // IPS_SetPosition($id, $position);
         
         if (!IPS_EventExists($id)) throw new Exception("Event $ident could not be created."); 
 
@@ -340,11 +340,11 @@ class IPSEventTrigger extends IPSEvent
     {
         $id = parent::RegisterEvent($name, $position);
         
-        $this->Hide();
-        $this->SetScript($script);
-        $this->SetTrigger($targetId, $type); 
-        $this->SetSubsequentExecution(true);
-        $this->Activate();
+        // $this->Hide();
+        // $this->SetScript($script);
+        // $this->SetTrigger($targetId, $type); 
+        // $this->SetSubsequentExecution(true);
+        // $this->Activate();
 
         return $id;
     }
