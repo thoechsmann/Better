ccac2697b1161c427a6da802de88ea2ab101905f
Thomas Hoechsmann <thoechsmann@iwef.de>
2016-10-09 17:36:22 +0200

    fix

--- BetterHeatingNew/module.php
+++ BetterHeatingNew/module.php
@@ -159,11 +159,11 @@ class BetterHeatingNew extends BetterBase {
             $windowId = $this->WindowStatusIds()->ValueAt($i);
             if($windowId != 0)
             {
-                $this->WindowStatusTrigger($i).Register($windowId, 'BHN_UpdateWindow($_IPS[\'TARGET\']);');
+                $this->WindowStatusTrigger($i)->Register($windowId, 'BHN_UpdateWindow($_IPS[\'TARGET\']);', IPSEventTrigger::TypeChange);
             }
         }
 
-        $this->ModeTrigger()->Register($this->ModeId()->Value(), 'BHN_UpdateHeatingMode($_IPS[\'TARGET\']);');
+        $this->ModeTrigger()->Register($this->ModeId()->Value(), 'BHN_UpdateHeatingMode($_IPS[\'TARGET\']);', IPSEventTrigger::TypeUpdate);
 
         $this->UpdateWindow();
         $this->UpdateHeatingMode();
